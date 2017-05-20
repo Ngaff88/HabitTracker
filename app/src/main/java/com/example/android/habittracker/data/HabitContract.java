b4298190cda9ba6629af11752ed59e7445526342
Ngaff88 <Ngaff88@gmail.com>
2017-05-20 16:22:33 -0400

    Slight Edit to Contract

--- app/src/main/java/com/example/android/habittracker/data/HabitContract.java
+++ app/src/main/java/com/example/android/habittracker/data/HabitContract.java
@@ -12,9 +12,9 @@ public class HabitContract {
 
         public static final String Table_Name = "habits";
         public static final String _ID = BaseColumns._ID;
-        public static final String Column_Habit_Name = "name";
-        public static final String Column_Habit_Description = "description";
-        public static final String Column_Habit_Due = "date due";
+        public static final String Column_Habit_Name = "Name";
+        public static final String Column_Habit_Description = "Description";
+        public static final String Column_Habit_Due = "When it's Due";
 
 
         //Possible Choices for Dates
